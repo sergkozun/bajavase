6da69b856a6d6d705faef2c2d40fc180ba62b81d
sergkozun <86195209+sergkozun@users.noreply.github.com>
2021-06-20 22:41:07 +0600

    all methods has been implemented!

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,51 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j = i; j < size; j++) {
+                    int next = j + 1;
+                    storage[j] = storage[next];
+                    storage[next] = null;
+                }
+                size--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] result = new Resume[size];
+        System.arraycopy(storage, 0, result, 0, size);
+        return result;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
